3a95fe095acaabc4c15dab8375b2d7005e99ca2d
tnakamura <griefworker@gmail.com>
2021-01-20 15:53:30 +0900

    Add TokenResponse

---
+++ src/HatenaBookmarkSharp/OAuth/TokenResponse.cs
@@ -0,0 +1,18 @@
+﻿#nullable enable
+using System.Linq;
+
+namespace HatenaBookmarkSharp.OAuth
+{
+    public sealed class TokenResponse<T> where T : Token
+    {
+        public T Token { get; }
+
+        public ILookup<string, string> ExtraData { get; }
+
+        public TokenResponse(T token, ILookup<string, string> extraData)
+        {
+            Token = token;
+            ExtraData = extraData;
+        }
+    }
+}
