40ace18e43e23c61975bee61e6288aa5dcd460fd
tnakamura <griefworker@gmail.com>
2021-01-20 16:00:33 +0900

    Add OAuthUtility

---
+++ src/HatenaBookmarkSharp/OAuth/OAuthUtility.cs
@@ -0,0 +1,93 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace HatenaBookmarkSharp.OAuth
+{
+    public static class OAuthUtility
+    {
+        public delegate byte[] HashFunction(byte[] key, byte[] buffer);
+
+        static readonly Random random = new Random();
+
+        public static HashFunction? ComputeHash { private get; set; }
+
+        public static IEnumerable<KeyValuePair<string, string>> BuildBasicParameters(
+            string consumerKey,
+            string consumerSecret,
+            string url,
+            HttpMethod method,
+            Token? token = null,
+            IEnumerable<KeyValuePair<string, string>>? optionalParameters = null)
+        {
+            if (url == null) throw new ArgumentNullException(nameof(url));
+
+            var parameters = new List<KeyValuePair<string, string>>(capacity: 7)
+            {
+                new KeyValuePair<string,string>("oauth_consumer_key", consumerKey),
+                new KeyValuePair<string,string>("oauth_nonce", random.Next().ToString() ),
+                new KeyValuePair<string,string>("oauth_timestamp", DateTime.UtcNow.ToUnixTime().ToString() ),
+                new KeyValuePair<string,string>("oauth_signature_method", "HMAC-SHA1" ),
+                new KeyValuePair<string,string>("oauth_version", "1.0" )
+            };
+            if (token != null)
+            {
+                parameters.Add(new KeyValuePair<string, string>("oauth_token", token.Key));
+            }
+            if (optionalParameters == null)
+            {
+                optionalParameters = Enumerable.Empty<KeyValuePair<string, string>>();
+            }
+
+            var signature = GenerateSignature(
+                consumerSecret,
+                new Uri(url),
+                method,
+                token,
+                parameters.Concat(optionalParameters));
+
+            parameters.Add(new KeyValuePair<string, string>("oauth_signature", signature));
+
+            return parameters;
+        }
+
+        static string GenerateSignature(
+            string consumerSecret,
+            Uri uri,
+            HttpMethod method,
+            Token? token,
+            IEnumerable<KeyValuePair<string, string>> parameters)
+        {
+            if (ComputeHash == null)
+            {
+                throw new InvalidOperationException(
+                    "ComputeHash is null, must initialize before call OAuthUtility.HashFunction = /* your computeHash code */ at once.");
+            }
+
+            var hmacKeyBase = consumerSecret.UrlEncode() +
+                "&" +
+                ((token == null) ? "" : token.Secret).UrlEncode();
+
+            // escaped => unescaped[]
+            var queryParams = Utility.ParseQueryString(uri.GetComponents(UriComponents.Query | UriComponents.KeepDelimiter, UriFormat.UriEscaped));
+
+            var stringParameter = parameters
+                .Where(x => x.Key.ToLower() != "realm")
+                .Concat(queryParams)
+                .Select(p => new { Key = p.Key.UrlEncode(), Value = p.Value.UrlEncode() })
+                .OrderBy(p => p.Key, StringComparer.Ordinal)
+                .ThenBy(p => p.Value, StringComparer.Ordinal)
+                .Select(p => p.Key + "=" + p.Value)
+                .ToString("&");
+            var signatureBase = method.ToString() +
+                "&" + uri.GetComponents(UriComponents.SchemeAndServer | UriComponents.Path, UriFormat.Unescaped).UrlEncode() +
+                "&" + stringParameter.UrlEncode();
+
+            var hash = ComputeHash(Encoding.UTF8.GetBytes(hmacKeyBase), Encoding.UTF8.GetBytes(signatureBase));
+            return Convert.ToBase64String(hash).UrlEncode();
+        }
+    }
+}
