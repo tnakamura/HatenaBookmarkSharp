cd01a0a0e4cb50f13dffd38f866d43d40c1df12c
tnakamura <griefworker@gmail.com>
2021-01-20 15:51:38 +0900

    Add Token

---
+++ src/HatenaBookmarkSharp/OAuth/Token.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HatenaBookmarkSharp.OAuth
+{
+    public abstract class Token
+    {
+        public string Key { get; private set; }
+
+        public string Secret { get; private set; }
+
+        protected Token(string key, string secret)
+        {
+            Key = key ?? throw new ArgumentNullException(nameof(key));
+            Secret = secret ?? throw new ArgumentNullException(nameof(secret));
+        }
+    }
+
+    public sealed class AccessToken : Token
+    {
+        public AccessToken(string key, string secret)
+            : base(key, secret)
+        {
+        }
+    }
+
+    public sealed class RequestToken : Token
+    {
+        public RequestToken(string key, string secret)
+            : base(key, secret)
+        {
+        }
+    }
+}
